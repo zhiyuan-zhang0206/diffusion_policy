830a820c574883bac3169458a6b74f8918bfe788
zhiyuan-zhang0206 <wuji.zhangagain@gmail.com>
2024-07-28 22:05:42 -0700

    trainable on slurm

--- train.sh
+++ train.sh
@@ -1,35 +1,35 @@
-# cwd=$(pwd)
+username=`whoami`
 
-# cd /home/zzy/robot/robot_zzy/diffusion_policy
-# conda activate robodiff
-# export DATA_ROOT=/home/zzy/robot/data/diffusion_policy_data/data
-# export HYDRA_FULL_ERROR=1
-
-# python train.py --config-dir=. --config-name=image_pusht_diffusion_policy_cnn.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='/home/zzy/robot/data/diffusion_policy_data/data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
-
-# unset DATA_ROOT
-# unset HYDRA_FULL_ERROR
-# conda deactivate
-# cd ${cwd}
-
-
-# # . /home/zzy/robot/robot_zzy/diffusion_policy/train.sh
-
-
-
-#!/bin/bash
+if [ "$username" = "zzy" ]; then
+    project_code_root="/home/zzy/robot/robot_zzy"
+    data_root="/home/zzy/robot/data/diffusion_policy_data/data"
+    is_slurm=false
+elif [ "$username" = "zhangzhiyuan" ]; then
+    project_code_root="/scratch/zhangzhiyuan/robot_zzy"
+    data_root="/scratch/zhangzhiyuan/data/diffusion_policy_data/data"
+    is_slurm=true
+elif [ "$username" = "zhiyuan" ]; then
+    project_code_root="$HOME/robot/robot_zzy"
+    data_root="$HOME/robot/data/diffusion_policy_data/data"
+    is_slurm=false
+    home_path=$(echo $HOME)
+    if [[ "$home_path" == *"mnt/lustre"* ]]; then
+        is_slurm=true
+    fi
+fi
 
 export HYDRA_FULL_ERROR=1
-export DATA_ROOT=/home/zzy/robot/data/diffusion_policy_data/data
+export DATA_ROOT=$data_root
 export ZZY_DEBUG=True
 
-PYTHON_PATH=/home/zzy/miniconda3/envs/robodiff/bin/python
-SCRIPT_PATH=/home/zzy/robot/robot_zzy/diffusion_policy/train.py
-CWD=/home/zzy/robot/robot_zzy/diffusion_policy
-
-cd $CWD
-
-$PYTHON_PATH $SCRIPT_PATH \
-    --config-dir=. \
-    --config-name=train_ae_workspace.yaml \
-    "hydra.run.dir='/home/zzy/robot/data/diffusion_policy_data/data/outputs/\${now:%Y.%m.%d}/\${now:%H.%M.%S}_\${name}_\${task_name}'"
+if [ "$is_slurm" = "true" ]; then
+    srun python train.py \
+        --config-dir=. \
+        --config-name=train_ae_workspace.yaml \
+        "hydra.run.dir='${DATA_ROOT}/outputs/\${now:%Y.%m.%d}/\${now:%H.%M.%S}_\${name}_\${task_name}'"
+else
+    python train.py \
+        --config-dir=. \
+        --config-name=train_ae_workspace.yaml \
+        "hydra.run.dir='${DATA_ROOT}/outputs/\${now:%Y.%m.%d}/\${now:%H.%M.%S}_\${name}_\${task_name}'"
+fi
